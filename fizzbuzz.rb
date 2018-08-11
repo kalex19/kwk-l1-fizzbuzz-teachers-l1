a2de47b887ff2669f9c4bfeca6f40b3d0b098540
kalex19 <klewis@rockymountainprep.org>
2018-08-11 12:25:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 
 def num 
 
-if num % 3 == 0 && num % 5 == 0
-return "fizzbuzz"
-elsif num % 3 == 0 
-return "fizz"
-else num % 5 == 0 
-return "buzz"
-end
+  if num % 3 == 0 && num % 5 == 0
+  return "fizzbuzz"
+  elsif num % 3 == 0 
+  return "fizz"
+  else num % 5 == 0 
+  return "buzz"
+  end
 end
 num(6)
